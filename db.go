8ac9125362f4ad947e0c2c60f6fe20f3edadb186
Oleg Dobrev <oleg.dobrev.99@mail.ru>
2020-11-18 21:59:25 +0200

    t1

--- db.go
+++ db.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
+	_ "github.com/lib/pq"
+	"os"
 	_ "os"
 )
 
@@ -20,12 +21,7 @@ const (
 )
 func dbConnect() error {
 	var err error
-	db, err = sql.Open("postgres", fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=require",
-		DbUser,
-		DbPassword,
-		"localhost",
-		"5432",
-		DbName))
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		return err
 	}
